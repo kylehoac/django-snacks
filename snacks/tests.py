01a4c4dd5b376259d3f2a1852d9341aaa13d61fa
Kyle Hoac <hoackyle@gmail.com>
2021-06-23 17:44:08 -0700

    finished lab 28

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,7 +1,20 @@
-from django.test import SimpleTestCase
+from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth import get_user_model
+from .models import Snacks
 
-class PagesTest(SimpleTestCase):
+class SnacksTest(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="tester",
+            email="test@email.com",
+            password="password1234"
+        )
+        self.snacks = Snacks.objects.create(
+            name="pickle",
+            purchaser= self.user,
+            description="nice"
+        )
     def test_home_page_200(self):
         url = reverse('home')
         response = self.client.get(url)
@@ -30,8 +43,33 @@ class PagesTest(SimpleTestCase):
         self.assertTemplateUsed(response, "base.html")
 
     def test_faq_page_template_used(self):
-        url = reverse('about')
+        url = reverse('faq')
         response = self.client.get(url)
         self.assertTemplateUsed(response, "faq.html")
         self.assertTemplateUsed(response, "base.html")
 
+    def test_snack_list_view(self):
+        response = self.client.get(reverse("snacks_list"))
+        self.assertContains(response, "pickle")
+
+    def test_snack_detail_view(self):
+        response = self.client.get(reverse("snacks_detail", args="1"))
+        no_response = self.client.get("/100000/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, "Purchaser:tester")
+        self.assertTemplateUsed((response, "snacks_detail.html"))
+
+    def test_snack_create_view(self):
+        response = self.client.post(
+            reverse("snacks_create"),
+            {
+                "name": "Ice Cream",
+                "purchaser": self.user.id,
+                "description": "yum",
+            }, follow=True
+        )
+
+        self.assertRedirects(response, reverse("snacks_detail",
+        args="2"))
+        self.assertContains(response, "Description:yum")
--- snacks/urls.py
+++ snacks/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import AboutPageView, HomePageView, FAQPageView, SnacksListView,SnacksDetailView
+from .views import AboutPageView, HomePageView, FAQPageView, SnacksListView,SnacksDetailView,SnacksCreateView,SnacksUpdateView,SnacksDeleteView
 
 urlpatterns = [
     path('home/', HomePageView.as_view(), name='home'),
@@ -7,4 +7,7 @@ urlpatterns = [
     path('faq/', FAQPageView.as_view(), name='faq'),
     path('', SnacksListView.as_view(), name='snacks_list'),
     path('<int:pk>/', SnacksDetailView.as_view(), name='snacks_detail'),
+    path('create/', SnacksCreateView.as_view(), name ='snacks_create'),
+    path('<int:pk>/update/', SnacksUpdateView.as_view(), name ='snacks_update'),
+    path('<int:pk>/delete/', SnacksDeleteView.as_view(), name ='snacks_delete'),
 ]
